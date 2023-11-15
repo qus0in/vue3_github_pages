ec374424f422be9c9546865ab15efae186536224
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-11-15 17:33:52 +0845

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ command }) => {
+  if (command == "build") {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+      base: "/vue3_github_pages/", // Repository 이름
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
   }
-})
+});
